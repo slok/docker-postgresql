93d90535037061132f602a23ac797312dd730322
Xabier Larrakoetxea <slok69@gmail.com>
2015-01-01 18:42:48 +0100

    Small fix

--- postgresql/run.sh
+++ postgresql/run.sh
@@ -5,6 +5,7 @@ source /etc/default_user.sh
 POSTGRESQL_BIN=/usr/lib/postgresql/9.4/bin/postgres
 POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.4/main/postgresql.conf
 POSTGRESQL_DATA=/data
+POSTGRESQL_STATS=/var/run/postgresql/9.4-main.pg_stat_tmp
 
 POSTGRESQL_CMD="sudo -u postgres $POSTGRESQL_BIN --single --config-file=$POSTGRESQL_CONFIG_FILE"
 
@@ -21,6 +22,12 @@ if [ ! -d $POSTGRESQL_DATA ] || [ ${#files[@]} -eq 0 ]; then
     $POSTGRESQL_CMD <<< "CREATE USER $POSTGRESQL_USER WITH SUPERUSER;" > /dev/null
     $POSTGRESQL_CMD <<< "ALTER USER $POSTGRESQL_USER WITH PASSWORD '$POSTGRESQL_PASS';" > /dev/null
     $POSTGRESQL_CMD <<< "CREATE DATABASE $POSTGRESQL_DB OWNER $POSTGRESQL_USER TEMPLATE $POSTGRESQL_TEMPLATE;" > /dev/null
+fi
+
+if [ ! -d $POSTGRESQL_STATS ]; then
+    mkdir $POSTGRESQL_STATS
+    chown -R postgres:postgres $POSTGRESQL_STATS
+
 fi
 
 exec sudo -u postgres $POSTGRESQL_BIN  -D POSTGRESQL_DATA --config-file=$POSTGRESQL_CONFIG_FILE
